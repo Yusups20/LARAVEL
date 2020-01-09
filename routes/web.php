ee4f3bcc4e33538b329decf65138f1f96752545d
refan8881 <efan7151@gmail.com>
2020-01-09 11:34:43 +0700

    Belajar Laravel

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,22 @@ Route::get('pesan/{makan}/{minum}/{harga}',function($mkn,$mnm,$hrg){
     .'Minuman : '.$mnm.'<br>'
     .'Total Harga : '.$hrg;
 });
+
+//Route Optional Parameter
+Route::get('halo/{nama?}',function($nama='Ys'){
+    return 'Hello Nama Saya'.$nama;
+});
+
+Route::get('pesanan/{makan?}/{minum?}/{harga?}',function($mkn=null,$mnm=null,$hrg=null){
+    if  (isset($mkn)) {
+        echo "Anda Memesan Makan : ".$mkn;
+    }if (isset($mnm)) {
+        echo "Dan : ".$mnm;
+    }if (isset($hrg)) {
+        echo "Dengan Harga : ".$hrg;
+    }if ($mkn == null && $mnm == null && $hrg == null) {
+        echo "Anda Belum Memesan";
+    }if (isset($makan)) {
+        echo "Anda Memesan Makan : ".$makan;
+    }
+});
